6bbbf16e392aeb426a1f1b4c069cd7a76c06e070
sourav4125 <souravmisra4125@gmail.com>
2022-12-14 11:23:23 +0530

    post api

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,10 +6,9 @@ const _ = require("underscore");
 // 1st problem
 const name = require("../logger/logger");
 // 2nd problem
-const date=require("../util/helper")
+const date = require("../util/helper");
 // 3rd problem
-const convert= require("../validator/formatter")
-
+const convert = require("../validator/formatter");
 
 router.get("/test-me", function (req, res) {
   res.send("any dummy text");
@@ -17,65 +16,97 @@ router.get("/test-me", function (req, res) {
   name.welcome("Sourav");
   date.printDate();
   // date.printMonth()
-  date.getBatchInfo()
+  date.getBatchInfo();
   convert.trim(" Viper ");
   convert.changetoLowerCase("MUSTANG GT");
-  convert.changeToUpperCase('kratos')
+  convert.changeToUpperCase("kratos");
 
-
-  const month=["January","February","March","April","May","June","July","August","September","October","November","December"]
-  let result=_.chunk(month,4)
-  console.log(result)
-  const numbers=[1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-  let example=_.tail(numbers)
-  console.log(example)
-
-  
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let result = _.chunk(month, 4);
+  console.log(result);
+  const numbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+  let example = _.tail(numbers);
+  console.log(example);
 });
 
-router.get("/movies/:indexNumber",function(req,res){
+router.get("/movies/:indexNumber", function (req, res) {
+  const movies = [
+    "Jersey",
+    "Conjuring",
+    "Major",
+    "Death Race",
+    "Avengers",
+    "Spider-Man",
+    "Fast & Furious",
+    "Call Of Duty",
+    "Transformers",
+    "Kratos",
+    "Viper",
+    "Bramhastra",
+    "Hridayam",
+  ];
+  req.params.indexNumber <= movies.length
+    ? res.send(movies[req.params.indexNumber])
+    : res.send("Enter a valid index number");
+});
 
-  const movies = ['Jersey', 'Conjuring', 'Major', 'Death Race','Avengers','Spider-Man','Fast & Furious','Call Of Duty','Transformers','Kratos','Viper','Bramhastra','Hridayam']
-  req.params.indexNumber<=movies.length? res.send(movies[req.params.indexNumber]) : res.send("Enter a valid index number")
-
-})
-
-router.get("/films",function (req,res){
+router.get("/films", function (req, res) {
   const films = [
     {
       id: 1,
-      name: 'The Shawshank Redemption'
+      name: "The Shawshank Redemption",
     },
     {
       id: 2,
-      name: 'The Godfather'
+      name: "The Godfather",
     },
     {
       id: 3,
-      name: 'The Godfather: Part II'
+      name: "The Godfather: Part II",
     },
     {
       id: 4,
-      name: 'The Dark Knight'
+      name: "The Dark Knight",
     },
     {
       id: 5,
-      name: '12 Angry Men'
-    }
+      name: "12 Angry Men",
+    },
   ];
-  
-  res.send(films)
-  
- 
 
-
-})
-
-
+  res.send(films);
+});
 
 router.get("/test-you", function (req, res) {
   console.log("I am here");
   res.send("very important text");
+});
+
+// --------------------------------------->POST<--------------------------------------\\
+const players=[];
+router.post("/players", function (req, res) {
+let body=req.body;
+let playerinformation=players.find(players=>players.name===body.name) 
+if(playerinformation){
+  return res.send("Player has alreay been added")
+}else{
+  players.push(body)
+  return res.send(players)
+}
+
 });
 
 module.exports = router;
